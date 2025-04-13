94586ba28d83464efa2a5acb6b859bb836d3a29e
Chan531 <chan657312@gmail.com>
2025-04-13 20:45:02 +0900

    2일차 : 접근 제한자 설정

--- src/main/kotlin/com/practice/blog/controller/MemberController.kt
+++ src/main/kotlin/com/practice/blog/controller/MemberController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/members")
 class MemberController(
-    val memberService: MemberService
+    private val memberService: MemberService
 ) {
 
     @GetMapping("/{id}")
--- src/main/kotlin/com/practice/blog/service/MemberService.kt
+++ src/main/kotlin/com/practice/blog/service/MemberService.kt
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional(readOnly = true)
 class MemberService(
-    val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository
 ) {
 
     fun get(id: String): MemberGetResponse {
